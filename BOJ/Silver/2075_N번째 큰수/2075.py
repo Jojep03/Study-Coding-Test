67572677acf0a6d072168e98d830de7c76eb9b43
Jojep03 <ipone131217@gmail.com>
2026-03-12 21:05:37 +0900

    BOJ 2075 N번째 큰 수

---
+++ BOJ/Silver/2075_N번째 큰수/2075.py
@@ -0,0 +1,19 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+n = int(input())
+heap = []
+for _ in range(n):
+    row = list(map(int, input().split()))
+
+    for num in row:
+        if len(heap) < n:
+            heapq.heappush(heap, num)
+        else:
+            if num > heap[0]: #시간초과
+                heapq.heappop(heap)
+                heapq.heappush(heap, num)
+
+print(heap[0])
