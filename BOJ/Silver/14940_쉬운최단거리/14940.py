a5ae8e2f2d7d5417280306f96ee92025068158f0
Jojep03 <ipone131217@gmail.com>
2026-03-21 22:59:08 +0900

    BOJ14940_쉬운거리찾기

--- BOJ/Silver/14940_쉬운최단거리/14940.py
+++ BOJ/Silver/14940_쉬운최단거리/14940.py
@@ -3,27 +3,38 @@ from collections import deque,defaultdict
 
 n, m = map(int, input().split())
 
+sy, sx = 0, 0
 lst = []
-start = []
 for i in range(n):
     a = list(map(int, input().split()))
     lst.append(a)
     if 2 in a:
-        start.append((i, a.index(2)))
+        sy, sx = i, a.index(2)
 
-print(lst)
-print(start)
-def bfs(graph, start):
-    visited = [False] * (n + 1)
-    order = []
-    stack = deque([start])
+dist = [[-1] * m for _ in range(n)]
+# print(dist)
+dist[sy][sx] = 0
+q = deque()
+q.append((sy, sx))
+dy = [-1, 1, 0, 0]
+dx = [0, 0, -1, 1]
 
-    while stack:
-        node = stack.popleft()
-        if not visited[node]:
-            visited[node] = True
-            order.append(node)
-            for nxt in graph[node]:
-                if not visited[nxt]:
-                    stack.append(nxt)
-    return order
+while q:
+    y, x = q.popleft()
+
+    for k in range(4):
+        ny = y + dy[k]
+        nx = x + dx[k]
+
+        if 0 <= ny < n and 0 <= nx < m:
+            if dist[ny][nx] == -1 and lst[ny][nx] != 0:
+                dist[ny][nx] = dist[y][x] + 1
+                q.append((ny, nx))
+
+for i in range(n):
+    for j in range(m):
+        if lst[i][j] == 0:
+            print(0, end=' ')
+        else:
+            print(dist[i][j], end=' ')
+    print()
