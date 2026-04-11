ffe5aee40df035a6647e3d3ff390b328e323a984
Jojep03 <ipone131217@gmail.com>
2026-04-11 21:59:46 +0900

    BOJ1654_랜선자르기

---
+++ BOJ/Silver/1654_랜선자르기/1654.py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+
+k, n = map(int, input().split())
+lines = [int(input()) for _ in range(k)]
+
+start = 1
+end = max(lines)
+answer = 0
+
+while start <= end:
+    mid = (start + end) // 2
+    cnt = 0
+    for line in lines:
+        cnt += (line // mid)
+    if cnt >= n:
+        answer = mid
+        start = mid + 1
+    else:
+        end = mid - 1
+print(answer)
