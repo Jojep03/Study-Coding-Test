ef584a2915e5abc14fc6989b44efe679fd1574ba
Jojep03 <ipone131217@gmail.com>
2026-05-03 16:45:14 +0900

    BOJ7662_이중우선순위큐

--- BOJ/Gold/7662_이중우선순위큐/7662.py
+++ BOJ/Gold/7662_이중우선순위큐/7662.py
@@ -8,22 +8,35 @@ for _ in range(t):
     k = int(input())
     q_max = []
     q_min = []
-
-
-    for _ in range(k):
+    visited = [False] * k
+    for i in range(k):
         cmd, n = input().split()
         n = int(n)
+
         if cmd == "I":
-            heapq.heappush(q_max, -n)
-            heapq.heappush(q_min, n)
+            heapq.heappush(q_max, (-n, i))
+            heapq.heappush(q_min, (n, i))
+            visited[i] = True
+
         elif cmd == "D":
             if n == 1:
+                while q_max and not visited[q_max[0][1]]:
+                    heapq.heappop(q_max)
                 if q_max:
+                    visited[q_max[0][1]] = False
                     heapq.heappop(q_max)
             else:
-                if q_min:
+                while q_min and not visited[q_min[0][1]]:
                     heapq.heappop(q_min)
+                if q_min:
+                    visited[q_min[0][1]] = False
+                    heapq.heappop(q_min)
+
+    while q_max and not visited[q_max[0][1]]:
+        heapq.heappop(q_max)
+    while q_min and not visited[q_min[0][1]]:
+        heapq.heappop(q_min)
     if q_max and q_min:
-        print(-heapq.heappop(q_max), heapq.heappop(q_min))
+        print(-q_max[0][0], q_min[0][0])
     else:
         print("EMPTY")
