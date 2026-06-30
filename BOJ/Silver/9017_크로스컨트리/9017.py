99abf12ea5ae276089f1240dd7b390be2abeba29
Jojep03 <ipone131217@gmail.com>
2026-06-30 20:01:15 +0900

    BOJ9017_크로스컨트리

--- BOJ/Silver/9017_크로스컨트리/9017.py
+++ BOJ/Silver/9017_크로스컨트리/9017.py
@@ -8,10 +8,27 @@ for _ in range(t):
     players = list(map(int, input().split()))
     x = set()
     y = []
+    # for player in players:
+    #     if players.count(player) == 6:
+    #         x.add(player)
+    #         y.append(player)
+    counter = Counter(players)
+    # print(counter)
     for player in players:
-        if players.count(player) == 6:
-            x.add(player)
+        if counter[player] == 6:
             y.append(player)
-    for i in x:
-        for j in range(len(y)):
-            if y[j] == i:
+    lst = {}
+    idx = 1
+    for player in y:
+        if player in lst:
+            if lst[player][0] < 4:
+                lst[player][0] += 1
+                lst[player][1] += idx
+            elif lst[player][0] == 4:
+                lst[player][0] += 1
+                lst[player][2] = idx
+        else:
+            lst[player] = [1, idx, 0]
+        idx += 1
+    lst = sorted(lst.items(), key=lambda x: (x[1][1], x[1][2]))
+    print(lst[0][0])
