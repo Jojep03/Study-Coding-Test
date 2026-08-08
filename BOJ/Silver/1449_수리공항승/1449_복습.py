3de36a6082b26dcd85cf27d6f7bb6aa4f08d3395
Jojep03 <ipone131217@gmail.com>
2026-08-08 19:56:32 +0900

    BOJ1449_복습

---
+++ BOJ/Silver/1449_수리공항승/1449_복습.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+n, l = map(int, input().split())
+lst = list(map(int, input().split()))
+lst.sort()
+cnt = 0
+end = 0
+for x in lst:
+    if x > end:
+        cnt += 1
+        end = x + l - 1
+print(cnt)
