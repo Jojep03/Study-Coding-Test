4269caad977134a3e2e18fe670237e9aab2a48ed
Jojep03 <ipone131217@gmail.com>
2026-08-11 20:22:43 +0900

    BOJ2217_복습

---
+++ BOJ/Silver/2217_로프/2217_복습.py
@@ -0,0 +1,11 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+lst = [int(input()) for _ in range(n)]
+lst.sort(reverse=True)
+res = 0
+for i in range(n):
+    res = max(res, lst[i] * (i + 1))
+print(res)
